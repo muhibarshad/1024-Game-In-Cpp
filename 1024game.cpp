d6e06ba1037b41a17f73a73228baf5c814e3b637
muhib7353 <98839666+muhib7353@users.noreply.github.com>
2022-06-04 14:56:05 +0500

    comments added
    Adding comments to help the user to find how each part the code works and what's is the function of it

--- 1024game.cpp
+++ 1024game.cpp
@@ -1,22 +1,29 @@
 //This game is developed by Muhib Arshad_(muhib7353)
+//Librarires
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <conio.h>
 #include <iomanip>
+
 using namespace std;
-void moveup();
+
+//global variables
 const int row = 4;
 const int col = 4;
 int main()
 {
 
+  //local varaibles
     bool flag = false;
     int count = 0;
     int matrix[row][col];
     int y = time(0);
     srand(y);
     char startgame;
+
+    //Initlizing the grid to 0
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
@@ -24,21 +31,28 @@ int main()
             matrix[i][j] = 0;
         }
     }
+    
+    //generating the random number at four sides up,down,left and right
     int rando_startCol = rand() % 4;
     int rando_startRow = rand() % 4;
     int rando_startCol2 = rand() % 4;
     int rando_startRow2 = rand() % 4;
     matrix[rando_startRow][rando_startCol] = (rand() % 2) + 1;
     matrix[rando_startRow2][rando_startCol2] = (rand() % 2) + 1;
+
+    //Spaces line
     for (int i = 0; i < 5; i++)
     {
         cout << endl;
     }
+
+    //When the game start How it look like
     cout << "\t\t\t\t\t To start the game!" << endl;
     cout << endl;
     cout << "\t\t\t\t\t Enter the S:";
     startgame = getch();
     startgame = tolower(startgame);
+    //Input validation
     while (startgame != 's')
     {
         cout << endl;
@@ -49,21 +63,31 @@ int main()
         startgame = getch();
     }
     startgame = tolower(startgame);
+
+    //Game start here at S input
     if (startgame == 's')
     {
+        //To clear the screen cls command to console
         system("cls");
+
+        //Do while can be used firstly play the game 
         do
         {
+            //Spaces line
             for (int i = 0; i < 5; i++)
             {
                 cout << endl;
             }
+            
+            //Instructions to the player
             cout << "\t\t\t\t\tEnter W to move upward:" << endl;
             cout << "\t\t\t\t\tEnter S to move downward:" << endl;
             cout << "\t\t\t\t\tEnter A to move left:" << endl;
             cout << "\t\t\t\t\tEnter D to move right:" << endl;
             cout << "\t\t\t\t\tEnter Q to quit the game:" << endl;
             cout << endl;
+
+            //Grid making deployation
             for (int i = 0; i < row; i++)
             {
                 cout << "\t\t\t\t ------------------------------------------" << endl;
@@ -77,14 +101,21 @@ int main()
                 cout << endl;
             }
             cout << "\t\t\t\t -------------------------------------------" << endl;
+
+            //Local input command variable
             char ch;
+
+            ///space lines
             for (int i = 0; i < 3; i++)
             {
                 cout << endl;
             }
+
+            //inPut
             cout << "\t\t\t\t\tEnter the valid key:";
             ch = getch();
             ch = tolower(ch);
+            //Input Validation
             while (ch != 'd' && ch != 'a' && ch != 'w' && ch != 's')
             {
                 cout << endl;
@@ -94,10 +125,13 @@ int main()
                 ch = getch();
             }
             ch = tolower(ch);
+
+            //Switch cases used to exexuate that command on every input
             switch (ch)
             {
             case 'd':
             {
+                //For Left moving of the numbers
                 system("cls");
                 for (int i = 0; i < col - 1; i++)
                 {
@@ -105,17 +139,24 @@ int main()
                     {
                         if (matrix[j][i] == matrix[j][i + 1])
                         {
+                            //Add the numbers if they are same
                             matrix[j][i + 1] += matrix[j][i];
+
+                            //If user win the game 
                             if (matrix[j][i + 1] == 1024)
                             {
                                 for (int i = 0; i < 10; i++)
                                 {
                                     cout << endl;
                                 }
+
+                                // flag become true and game stops
                                 flag = true;
                                 cout << "\t\t\t\t\tYou Win the game!:" << endl;
                             }
                             matrix[j][i] = 0;
+
+                            //Move the numbers all to the left side
                             for (int k = 0; k < row; k++)
                             {
                                 for (int l = 0; l < col - 1; l++)
@@ -133,6 +174,7 @@ int main()
                     }
                 }
 
+//generate the random number after the movement of the numbers
                 int randomrow;
                 int randomcol;
                 randomcol = rand() % 4;
@@ -157,6 +199,7 @@ int main()
                     matrix[randomrow][randomcol] = (rand() % 2) + 1;
                 }
 
+//Check if all the grid is full and random number find no place to sit then game beacme over 
                 int gameover = 0;
                 for (int i = 0; i < row; i++)
                 {
@@ -175,6 +218,8 @@ int main()
                 }
                 break;
             }
+
+            // For right moving of the numbers
             case 'a':
             {
                 system("cls");
@@ -182,19 +227,26 @@ int main()
                 {
                     for (int j = 0; j < row; j++)
                     {
+                        // Add the numbers if they are same
                         if (matrix[j][i] == matrix[j][i - 1])
                         {
                             matrix[j][i - 1] += matrix[j][i];
+
+                            // If user win the game
                             if (matrix[j][i - 1] == 1024)
                             {
                                 for (int i = 0; i < 10; i++)
                                 {
                                     cout << endl;
                                 }
+
+                                // flag become true and game stops
                                 flag = true;
                                 cout << "\t\t\t\t\tYou Win the game!:" << endl;
                             }
                             matrix[j][i] = 0;
+
+                            // Move the numbers all to the Right side
                             for (int k = 0; k < row; k++)
                             {
                                 for (int l = col - 1; l > 0; l--)
@@ -212,6 +264,7 @@ int main()
                     }
                 }
 
+                // generate the random number after the movement of the numbers
                 int randomrow;
                 int randomcol;
                 randomcol = rand() % 4;
@@ -236,6 +289,7 @@ int main()
                     matrix[randomrow][randomcol] = (rand() % 2) + 1;
                 }
 
+                // Check if all the grid is full and random number find no place to sit then game beacme over
                 int gameover = 0;
                 for (int i = 0; i < row; i++)
                 {
@@ -254,6 +308,8 @@ int main()
                 }
                 break;
             }
+
+            // For upward moving of the numbers
             case 'w':
             {
                 system("cls");
@@ -263,6 +319,8 @@ int main()
                     {
                         if (matrix[i][j] == matrix[i - 1][j])
                         {
+
+                            // Add the numbers if they are same
                             matrix[i - 1][j] += matrix[i][j];
                             if (matrix[i - 1][j] == 1024)
                             {
@@ -270,10 +328,14 @@ int main()
                                 {
                                     cout << endl;
                                 }
+
+                                // flag become true and game stops
                                 flag = true;
                                 cout << "\t\t\t\t\tYou Win the game!:" << endl;
                             }
                             matrix[i][j] = 0;
+
+                            // Move the numbers all to the upwaed side
                             for (int k = 0; k < col; k++)
                             {
                                 for (int l = row - 1; l > 0; l--)
@@ -291,6 +353,7 @@ int main()
                     }
                 }
 
+                // generate the random number after the movement of the numbers
                 int randomrow;
                 int randomcol;
                 randomcol = rand() % 4;
@@ -315,6 +378,7 @@ int main()
                     matrix[randomrow][randomcol] = (rand() % 2) + 1;
                 }
 
+                // Check if all the grid is full and random number find no place to sit then game beacme over
                 int gameover = 0;
                 for (int i = 0; i < row; i++)
                 {
@@ -333,6 +397,8 @@ int main()
                 }
                 break;
             }
+
+            // For downnward moving of the numbers
             case 's':
             {
                 system("cls");
@@ -340,6 +406,8 @@ int main()
                 {
                     for (int j = 0; j < col; j++)
                     {
+
+                        // Add the numbers if they are same
                         if (matrix[i][j] == matrix[i + 1][j])
                         {
                             matrix[i + 1][j] += matrix[i][j];
@@ -349,10 +417,14 @@ int main()
                                 {
                                     cout << endl;
                                 }
+
+                                // flag become true and game stops
                                 flag = true;
                                 cout << "\t\t\t\t\tYou Win the game!:" << endl;
                             }
                             matrix[i][j] = 0;
+
+                            // Move the numbers all to the downward side
                             for (int k = 0; k < col; k++)
                             {
                                 for (int l = 0; l < row - 1; l++)
@@ -370,6 +442,7 @@ int main()
                     }
                 }
 
+                // generate the random number after the movement of the numbers
                 int randomrow;
                 int randomcol;
                 randomcol = rand() % 4;
@@ -394,6 +467,7 @@ int main()
                     matrix[randomrow][randomcol] = (rand() % 2) + 1;
                 }
 
+                // Check if all the grid is full and random number find no place to sit then game beacme over
                 int gameover = 0;
                 for (int i = 0; i < row; i++)
                 {
@@ -414,7 +488,7 @@ int main()
             }
             }
 
-        } while (flag == false);
+        } while (flag == false);//If falg is false continue it to again 
     }
 
     return 0;
